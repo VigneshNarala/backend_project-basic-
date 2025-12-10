76c922d008c70b8fb9f1589718853d94dbca4de8
VigneshNarala <vigneshnarala135@gmail.com>
2025-12-10 21:14:35 +0530

    ..

--- Controller/studController.js
+++ Controller/studController.js
@@ -1,4 +1,10 @@
-import data from '..'
+import data from '../MOCK_DATA.json' assert { type: 'json' };
+
 const getstud = (req, res) => {
-    let studata = '';
+    let studata = data;
+    res.status(200).json(studata);
 };
+const addstud = (req, res) => {
+    
+};
+export { getstud };
